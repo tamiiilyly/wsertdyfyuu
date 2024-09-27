630ee8ca7c7e4446f7f5725c1879d82b8c80fe6d
tamiiilyly <161335688+tamiiilyly@users.noreply.github.com>
2024-09-27 09:15:31 -0300

    .

--- main.js
+++ main.js
@@ -9,6 +9,6 @@ function areaQuadrado(lado){
 return lado**2;
 }
 console.log (areaQuadrado (6));
-let media =function (num1,num2,num3,num4) {
-return (num1+num2+num3/num4)
+let media =function (num1,num2,num3) {
+return (num1+num2+num3)/num4
 }
