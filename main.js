b8e22f16ef71280a7ad20151b81a25f9483719c6
tamiiilyly <161335688+tamiiilyly@users.noreply.github.com>
2024-09-27 09:09:20 -0300

    .

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ console.log (soma(5,3));
 function mutiplicação (num1,num2) {
 return num1*num2;
 }
+function areaQuadrado(lado){
+return lado**2;
+}
+console.log (areaQuadrado (6));
+let media =function (num1,num2,num3,num4) {
+return (num1+num2+num3/num4)
+}
